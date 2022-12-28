76f3cabc82460e29acc17f4314cdeacb3ac7d433
Yunus Emre <dmrlyunusemre@gmail.com>
2022-12-28 19:42:22 +0300

    button-click

--- use_button/lib/main.dart
+++ use_button/lib/main.dart
@@ -4,6 +4,21 @@ void main() {
   runApp(const MyApp());
 }
 
+//DropDownMenu icin olusturuldu.
+const List<String> list = <String>[
+  '1.Secim',
+  '2.Secim',
+  '3.Secim',
+  '4.Secim',
+  '5.Secim'
+];
+String dropdownValue = list.first;
+
+//Popup Menu icin olusturuldu.
+enum SampleItem { itemOne, itemTwo, itemThree }
+
+SampleItem? selectedMenu;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -45,7 +60,9 @@ class _FirstPageState extends State<FirstPage> {
           padding: const EdgeInsets.all(5.0),
           child: FloatingActionButton(
             backgroundColor: Colors.amber,
-            onPressed: () {},
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+            },
             tooltip: 'Floating Button',
             child: const Icon(
               Icons.mouse,
@@ -62,29 +79,43 @@ class _FirstPageState extends State<FirstPage> {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
+        children: [
+          const Text(
             'Flutter Button Example',
           ),
           TextButton(
-            onPressed: null,
-            child: Text(
+            //Arka renk siyah. Ancak basilirken kirmizi.
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.red;
+              }
+              return Colors.black;
+            })),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+            },
+            child: const Text(
               'Text Button',
-              style: TextStyle(color: Colors.teal),
+              style: TextStyle(color: Colors.white),
             ),
           ),
           ElevatedButton(
-            onPressed: null,
-            child: Text(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+            },
+            child: const Text(
               'Elevated Button',
-              style: TextStyle(color: Colors.teal),
+              style: TextStyle(color: Colors.white),
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(15.0),
             child: InkWell(
-              onTap: null,
-              child: SizedBox(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+              },
+              child: const SizedBox(
                 height: 15,
                 child: Text(
                   'Inkwell Button',
@@ -93,10 +124,77 @@ class _FirstPageState extends State<FirstPage> {
               ),
             ),
           ),
-          IconButton(onPressed: null, icon: Icon(Icons.ads_click_rounded)),
-          OutlinedButton(onPressed: null, child: Text('Outline Button')),
+          IconButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+              },
+              icon: const Icon(Icons.ads_click_rounded)),
+          OutlinedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+              },
+              child: const Text('Outline Button')),
+          _dropDownButton(),
+          _popUpMenuButton()
         ],
       ),
     );
   }
+
+  _dropDownButton() {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      style: const TextStyle(color: Colors.teal),
+      onChanged: (String? value) {
+        {
+          setState(() {
+            dropdownValue = value!;
+          });
+          // ignore: avoid_print
+          print(dropdownValue);
+        }
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+
+  _popUpMenuButton() {
+    return PopupMenuButton<SampleItem>(
+      initialValue: selectedMenu,
+      onSelected: (SampleItem item) {
+        setState(() {
+          selectedMenu = item;
+        });
+      },
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+        const PopupMenuItem<SampleItem>(
+          value: SampleItem.itemOne,
+          child: Text('1.Secim'),
+        ),
+        const PopupMenuItem<SampleItem>(
+          value: SampleItem.itemTwo,
+          child: Text('2.Secim'),
+        ),
+        const PopupMenuItem<SampleItem>(
+          value: SampleItem.itemThree,
+          child: Text('3.Secim'),
+        ),
+      ],
+    );
+  }
 }
+
+const _snackBar = SnackBar(
+  content: Text(
+    'Butona Basildi.',
+  ),
+  backgroundColor: Colors.teal,
+  shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20))),
+);
