6335fbead9bb12c9714fd3249adf71b4003dda9b
Yunus Emre <dmrlyunusemre@gmail.com>
2024-05-29 18:11:48 +0300

    saveExcel

--- signature_app/lib/signature_view.dart
+++ signature_app/lib/signature_view.dart
@@ -1,10 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:signature/signature.dart';
+import 'package:excel/excel.dart';
+import 'package:image/image.dart' as img;
 import 'package:signature_app/signature_controller.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SignaturePage extends StatelessWidget {
-  const SignaturePage({super.key});
+  const SignaturePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +19,7 @@ class SignaturePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Imza At'),
+        title: const Text('Imza At'),
       ),
       body: Column(
         children: [
@@ -31,25 +38,73 @@ class SignaturePage extends StatelessWidget {
                 onPressed: () {
                   signaturePadController.clearSignature();
                 },
-                child: Text('Temizle'),
+                child: const Text('Temizle'),
               ),
               ElevatedButton(
                 onPressed: () async {
                   final signature =
                       await signaturePadController.exportSignature();
                   if (signature != null) {
-                    // İmzayı kaydet veya başka bir işlem yap
+                    await saveSignatureToExcel(signature);
                     print('İmza kaydedildi.');
                   } else {
                     print('İmza bulunamadı.');
                   }
                 },
-                child: Text('Kaydet'),
+                child: const Text('Kaydet'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final filePath =
+                      '/storage/emulated/0/mryed.xlsx'; // Dosyanın yeni yolu (dahili depolama ana dizini)
+                  final url =
+                      'file://$filePath'; // Dosya yolunu URL'ye dönüştür
+
+                  if (await canLaunch(url)) {
+                    await launch(url); // URL'yi aç
+                  } else {
+                    print('Excel uygulaması bulunamadı.');
+                  }
+                },
+                child: Text('Excel\'i Aç'),
               ),
             ],
           ),
         ],
       ),
     );
+  }
+
+  Future<void> saveSignatureToExcel(Uint8List signature) async {
+    var excel = Excel.createExcel();
+    var sheetObject = excel['Sayfa1'];
+
+    // İmzayı image paketi ile decode edin
+    img.Image? image = img.decodeImage(signature);
+    if (image == null) {
+      print('İmza resmi decode edilemedi.');
+      return;
+    }
+
+    // Image'i byte array olarak alın
+    var png = img.encodePng(image);
+
+    // Byte array'i base64 stringe çevirin
+    String base64Image = base64Encode(png);
+
+    // Base64 string'i Excel'e yazın
+    sheetObject.cell(CellIndex.indexByString("A5")).value =
+        TextCellValue(base64Image);
+
+    // Dosya kaydetme işlemi
+    final directory =
+        await getExternalStorageDirectory(); // Dahili depolama ana dizinine erişim sağlar
+    String filePath =
+        '${directory!.path}/mryed.xlsx'; // Dosyanın yeni yolu (dahili depolama ana dizini)
+    File(filePath)
+      ..createSync(recursive: true)
+      ..writeAsBytesSync(excel.encode()!);
+
+    print('Excel dosyası kaydedildi: $filePath');
   }
 }
